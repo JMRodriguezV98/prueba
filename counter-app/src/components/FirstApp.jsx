8baf727862560a39e1886fa92afc2145533b11ff
javier mauricio  rodriguez villamil <mriciosama@gmail.com>
2023-01-24 15:42:04 -0500

    esto es la subida de prueba

---
+++ counter-app/src/components/FirstApp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FirstApp = () => {
+    return (
+        <>
+            <h1>Mauricio</h1>
+            {/* <code>{ JSON.stringify( newMessage) }</code> */}
+            <p>Soy un subtitulo</p>
+        </>
+    );
+}
+
+export default FirstApp;
